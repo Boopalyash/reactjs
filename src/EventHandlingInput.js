5eee7983a2198bfb93212cefbb6818def667d66a
CIDC <cidc@CIDCs-Mac-mini.local>
2023-07-31 20:53:52 +0530

    updated as table using map

--- src/EventHandlingInput.js
+++ src/EventHandlingInput.js
@@ -1,4 +1,24 @@
 import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Grid,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Box,
+  Typography,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from "@mui/material";
+import moment from "moment";
 
 const EventHandlingInput = () => {
   const [firstName, setFirstName] = useState("");
@@ -8,10 +28,30 @@ const EventHandlingInput = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [gender, setGender] = useState("male");
   const [state, setState] = useState("tamilnadu");
-  const [city, setCity] = useState("namakkal");
-  const [consoleOutput, setConsoleOutput] = useState([]);
+  const [city, setCity] = useState("madurai");
   const [cityOptions, setCityOptions] = useState([]);
   const [errors, setErrors] = useState({});
+  const [serialNumber, setSerialNumber] = useState(0);
+  const [consoleOutput, setConsoleOutput] = useState([
+    {
+      serialNumber: 1,
+      firstName: "Sachin",
+      lastName: "Tendulkar",
+      email: "sachin@tendulkar.com",
+      phoneNumber: "1234567890",
+      time: moment().format("h:mm A"),
+      date: moment().format("DD-MM-YYYY"),
+    },
+    {
+      serialNumber: 2,
+      firstName: "Virat",
+      lastName: "Kolhi",
+      email: "virat@kolhi.com",
+      phoneNumber: "9876543210",
+      time: moment().format("h:mm A"),
+      date: moment().format("DD-MM-YYYY"),
+    },
+  ]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -39,18 +79,18 @@ const EventHandlingInput = () => {
     event.preventDefault();
     const isValid = validateForm();
     if (isValid) {
-      setConsoleOutput([
-        JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          password,
-          phoneNumber,
-          gender,
-          state,
-          city,
-        }),
-      ]);
+      const userData = {
+        serialNumber: serialNumber + 3,
+        firstName,
+        lastName,
+        email,
+        password,
+        phoneNumber,
+        time: moment().format("h:mm A"),
+        date: moment().format("DD-MM-YYYY"),
+      };
+      setConsoleOutput((prevOutput) => [...prevOutput, userData]);
+      setSerialNumber((prevSerialNumber) => prevSerialNumber + 1);
     }
   };
 
@@ -59,9 +99,8 @@ const EventHandlingInput = () => {
     const errors = {};
 
     // Validate First Name
-
-    if (!firstName.trim()) {
-      errors.firstName = "First Name is required ";
+    if (!firstName.length) {
+      errors.firstName = "First Name is required";
       isValid = false;
     } else if (firstName.length < 5) {
       errors.firstName = "Minimum 5 characters required";
@@ -84,11 +123,20 @@ const EventHandlingInput = () => {
     }
 
     // Validate Password
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasSymbol = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password);
+    const hasNumber = /\d/.test(password);
+
     if (!password.length) {
       errors.password = "Password is required";
       isValid = false;
     } else if (password.length < 6) {
       errors.password = "Password must be at least 6 characters long";
+      isValid = false;
+    } else if (!(hasUppercase && hasLowercase && hasSymbol && hasNumber)) {
+      errors.password =
+        "Password must contain at least one uppercase letter, one lowercase letter, one symbol, and one number";
       isValid = false;
     }
 
@@ -141,221 +189,192 @@ const EventHandlingInput = () => {
   }, [state]);
 
   return (
-    <div style={styles.container}>
-      <form onSubmit={handleSubmit} style={styles.form}>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>
-            First Name:
-            <input
-              type="text"
+    <Container>
+      <Box mt={4}>
+        <Typography variant="h4" align="center">
+          Personal Details
+        </Typography>
+      </Box>
+
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="First Name"
               name="firstName"
               value={firstName}
               onChange={handleChange}
-              style={styles.input}
-              required
+              error={!!errors.firstName}
+              helperText={errors.firstName}
             />
-          </label>
-          {errors.firstName && <p style={styles.error}>{errors.firstName}</p>}
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>
-            Last Name:
-            <input
-              type="text"
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Last Name"
               name="lastName"
               value={lastName}
               onChange={handleChange}
-              style={styles.input}
-              required
+              error={!!errors.lastName}
+              helperText={errors.lastName}
             />
-          </label>
-          {errors.lastName && <p style={styles.error}>{errors.lastName}</p>}
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>
-            Email:
-            <input
-              type="email"
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Email"
               name="email"
               value={email}
               onChange={handleChange}
-              style={styles.input}
-              required
+              error={!!errors.email}
+              helperText={errors.email}
             />
-          </label>
-          {errors.email && <p style={styles.error}>{errors.email}</p>}
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>
-            Password:
-            <input
-              type="password"
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Password"
               name="password"
+              type="password"
               value={password}
               onChange={handleChange}
-              style={styles.input}
-              required
+              error={!!errors.password}
+              helperText={errors.password}
             />
-          </label>
-          {errors.password && <p style={styles.error}>{errors.password}</p>}
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>
-            Phone Number:
-            <input
-              type="tel"
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Phone Number"
               name="phoneNumber"
               value={phoneNumber}
               onInput={handleInput}
-              style={styles.input}
-              required
+              error={!!errors.phoneNumber}
+              helperText={errors.phoneNumber}
             />
-          </label>
-          {errors.phoneNumber && (
-            <p style={styles.error}>{errors.phoneNumber}</p>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel>Gender</InputLabel>
+              <Select
+                label="Gender"
+                name="gender"
+                value={gender}
+                onChange={handleChange}
+              >
+                <MenuItem value="male">Male</MenuItem>
+                <MenuItem value="female">Female</MenuItem>
+                <MenuItem value="other">Other</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel>State</InputLabel>
+              <Select
+                label="State"
+                name="state"
+                value={state}
+                onChange={handleChange}
+              >
+                <MenuItem value="tamilnadu">Tamilnadu</MenuItem>
+                <MenuItem value="kerala">Kerala</MenuItem>
+                <MenuItem value="karnataka">Karnataka</MenuItem>
+                <MenuItem value="other">Other</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          {cityOptions.length > 0 && (
+            <Grid item xs={12} md={6}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel>City</InputLabel>
+                <Select
+                  label="City"
+                  name="city"
+                  value={city}
+                  onChange={handleChange}
+                >
+                  {cityOptions.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
           )}
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>
-            Gender:
-            <select
-              name="gender"
-              value={gender}
-              onChange={handleChange}
-              style={styles.input}
-            >
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-          </label>
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>State:</label>
-          <select
-            name="state"
-            value={state}
-            onChange={handleChange}
-            style={styles.input}
-          >
-            <option value="tamilnadu">Tamilnadu</option>
-            <option value="kerala">Kerala</option>
-            <option value="karnataka">Karnataka</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-
-        {cityOptions.length > 0 && (
-          <div style={styles.formGroup}>
-            <label style={styles.label}>City:</label>
-            <select
-              name="city"
-              value={city}
-              onChange={handleChange}
-              style={styles.input}
-            >
-              {cityOptions.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
-
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Other:</label>
-          <div>
-            <label>
-              <input
-                type="radio"
-                name="city"
-                value="namakkal"
-                checked={city === "namakkal"}
-                onChange={handleChange}
-                style={{ marginRight: "4px" }}
-              />
-              NAMAKKAL
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="city"
-                value="salem"
-                checked={city === "salem"}
-                onChange={handleChange}
-                style={{ marginRight: "4px", marginLeft: "10px" }}
-              />
-              SALEM
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="city"
-                value="karur"
-                checked={city === "karur"}
-                onChange={handleChange}
-                style={{ marginRight: "4px", marginLeft: "10px" }}
-              />
-              KARUR
-            </label>
-          </div>
-        </div>
-
-        <button type="submit" style={styles.button}>
-          Submit
-        </button>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
       </form>
-      <div style={styles.console}>
-        <h3> Output:</h3>
-        {consoleOutput.map((output, index) => (
-          <p key={index}>{output}</p>
-        ))}
-      </div>
-    </div>
+      <Box mt={4}>
+        <Typography variant="h6">Output:</Typography>
+        <TableContainer component={Paper} style={tableContainerStyle}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={tableCellStyle}>Serial Number</TableCell>
+                <TableCell style={tableCellStyle}>First Name</TableCell>
+                <TableCell style={tableCellStyle}>Last Name</TableCell>
+                <TableCell style={tableCellStyle}>Email</TableCell>
+                <TableCell style={tableCellStyle}>Phone Number</TableCell>
+                <TableCell style={tableCellStyle}>Date</TableCell>
+                <TableCell style={tableCellStyle}>Time</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {consoleOutput.map((userData, index) => (
+                <TableRow key={index}>
+                  <TableCell style={tableCellStyleNumber}>
+                    {userData.serialNumber}
+                  </TableCell>
+                  <TableCell style={tableCellStyle1}>
+                    {userData.firstName}
+                  </TableCell>
+                  <TableCell style={tableCellStyle1}>
+                    {userData.lastName}
+                  </TableCell>
+                  <TableCell style={tableCellStyle1}>
+                    {userData.email}
+                  </TableCell>
+                  <TableCell style={tableCellStyle1}>
+                    {userData.phoneNumber}
+                  </TableCell>
+                  <TableCell style={tableCellStyle1}>{userData.date}</TableCell>
+                  <TableCell style={tableCellStyle1}>{userData.time}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Container>
   );
 };
 
 export default EventHandlingInput;
-
-const styles = {
-  container: {
-    background: "linear-gradient(180deg, #80b3ff, #ff8080)",
-    minHeight: "100vh",
-  },
-  form: {
-    maxWidth: "300px",
-    margin: "0 auto",
-  },
-  formGroup: {
-    marginBottom: "15px",
-  },
-  label: {
-    fontSize: "14px",
-    fontWeight: "bold",
-  },
-  input: {
-    padding: "8px",
-    border: "1px solid #ccc",
-    borderRadius: "20px",
-    width: "100%",
-  },
-  button: {
-    marginTop: "10px",
-    padding: "10px",
-    backgroundColor: "#007bff",
-    color: "#fff",
-    border: "none",
-    borderRadius: "10px",
-    cursor: "pointer",
-  },
-  console: {
-    marginTop: "20px",
-    padding: "10px",
-    backgroundColor: "#f2f2f2",
-  },
-  error: {
-    color: "white",
-    fontSize: "15px",
-  },
+const tableContainerStyle = {
+  background: "linear-gradient(to top, #f2f2f2, #e0e0e0)",
+  padding: "10px",
+};
+const tableCellStyle = {
+  fontSize: "14px",
+  fontWeight: "bold",
+};
+const tableCellStyle1 = {
+  fontSize: "14px",
+  color: "red",
+};
+const tableCellStyleNumber = {
+  fontSize: "14px",
+  color: "blue",
 };
